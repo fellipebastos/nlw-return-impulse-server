756a2ebf7fe7f5685b4868f2737570fc87356e6b
fellipebastos <fellipebastoswm@gmail.com>
2022-05-09 11:48:10 -0300

    feat: finish project

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8937d7b3d48b3c",
+    pass: "118615b41d0168",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Fellipe Bastos <fellipebastoswm@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
